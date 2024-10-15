f650b17a92fadd85ea4be42de4cd278d1f2454e4
rgacorda <rgacorda.the2nd@gmail.com>
2024-10-15 09:49:09 +0800

    feat: added working map with lat/lng

--- src/modules/listing/components/FilterCard.tsx
+++ src/modules/listing/components/FilterCard.tsx
@@ -1,7 +1,12 @@
-import * as React from "react";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
 import { MultiSelect } from "@/components/multi-select";
+import React, { useState, useEffect, useRef} from 'react';
+import {
+	APIProvider,
+	Map
+} from '@vis.gl/react-google-maps'
+import PublicMap from "./PublicMap";
 
 const householdAmenities = [
   { value: "wifi", label: "WiFi" },
@@ -26,94 +31,38 @@ const householdAmenities = [
   { value: "towels", label: "Towels" },
 ];
 
-function loadGoogleMapsScript(callback) {
-  const existingScript = document.getElementById("googleMapsScript");
-  if (!existingScript) {
-    const script = document.createElement("script");
-    script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places`;
-    script.id = "googleMapsScript";
-    script.async = true;
-    script.defer = true;
-    document.body.appendChild(script);
-    script.onload = () => {
-      if (callback) callback();
-    };
-  } else if (callback) {
-    callback();
-  }
-}
-
 export function FilterCard() {
-  const mapRef = React.useRef(null);
-  const position = { lat: 16.415, lng: 120.597 };
 
-  const [selectedFilter, setSelectedFilter] = React.useState<string[]>([]);
-  const [pins, setPins] = React.useState<{ lat: number; lng: number }[]>([]);
-  const mapInstance = React.useRef(null);
+	const [selectedFilter, setSelectedFilter] = React.useState<string[]>([]);
 
-  React.useEffect(() => {
-    loadGoogleMapsScript(() => {
-      if (mapRef.current && window.google) {
-        const { Map, Marker } = window.google.maps;
-
-        const myLatlng = { lat: 16.415, lng: 120.597 };
-
-        const map = new Map(mapRef.current, {
-          zoom: 15,
-          center: myLatlng,
-          mapId: process.env.NEXT_PUBLIC_MAP_ID,
-        });
-        mapInstance.current = map;
-
-        map.addListener("click", (event) => {
-          if (event.latLng) {
-            const newPin = {
-              lat: event.latLng.lat(),
-              lng: event.latLng.lng(),
-            };
-            setPins((prevPins) => [...prevPins, newPin]);
-            console.log("Pinned Location:", newPin);
-
-
-
-            
-          } else {
-            console.error(
-              "Click event did not have latLng. Please ensure you are clicking directly on the map area."
-            );
-          }
-        });
-      } else {
-        console.error("Google Maps API is not available.");
-      }
-    });
-  }, [position, pins]);
-
-  return (
-    <Card className="w-full bg-white dark:bg-secondary shadow-md lg:mt-0 md:mt-4 sm:mt-4 xs:mt-4">
-      <CardHeader>
-        <Card className="h-[370px] border-none">
-          <div ref={mapRef} className="rounded-md w-full h-full border-none" />
-        </Card>
-      </CardHeader>
-      <CardContent>
-        <form>
-          <div>
-            <Label htmlFor="amenities" className="font-semibold">
-              Filter Amenities
-            </Label>
-            <MultiSelect
-              options={householdAmenities}
-              onValueChange={setSelectedFilter}
-              defaultValue={selectedFilter}
-              placeholder="Select amenities"
-              variant="inverted"
-              maxCount={6}
-            />
-          </div>
-        </form>
-      </CardContent>
-    </Card>
-  );
+	return (
+		<Card className='w-full bg-white dark:bg-secondary shadow-md lg:mt-0 md:mt-4 sm:mt-4 xs:mt-4'>
+			<CardHeader>
+				<Card className='h-[370px] border-none'>
+					<div className='rounded-md w-full h-full border-none'>
+						<PublicMap />
+					</div>
+				</Card>
+			</CardHeader>
+			<CardContent>
+				<form>
+					<div>
+						<Label htmlFor='amenities' className='font-semibold'>
+							Filter Amenities
+						</Label>
+						<MultiSelect
+							options={householdAmenities}
+							onValueChange={setSelectedFilter}
+							defaultValue={selectedFilter}
+							placeholder='Select amenities'
+							variant='inverted'
+							maxCount={6}
+						/>
+					</div>
+				</form>
+			</CardContent>
+		</Card>
+	);
 }
+
 
