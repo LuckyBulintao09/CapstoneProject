cce9fa14e8bd472dfbda1d29627cacb8cd212d1e
LuckyBulintao09 <luckybbulintao@gmail.com>
2024-10-20 21:39:52 +0800

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,42 +1,48 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
+import type { Metadata } from 'next';
+import localFont from 'next/font/local';
+import './globals.css';
 
-import { ThemeProvider } from "@/components/theme-provider";
-import { NUIProvider } from "@/components/next-ui-provider";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import NavBar from "@/components/navbar/Navbar";
+import { ThemeProvider } from '@/components/theme-provider';
+import { NUIProvider } from '@/components/next-ui-provider';
+import NavBar from '@/components/navbar/Navbar';
+import Footer from '@/modules/home/components/Footer';
 
 const geistSans = localFont({
-    src: "./fonts/GeistVF.woff",
-    variable: "--font-geist-sans",
-    weight: "100 900",
+	src: './fonts/GeistVF.woff',
+	variable: '--font-geist-sans',
+	weight: '100 900',
 });
 const geistMono = localFont({
-    src: "./fonts/GeistMonoVF.woff",
-    variable: "--font-geist-mono",
-    weight: "100 900",
+	src: './fonts/GeistMonoVF.woff',
+	variable: '--font-geist-mono',
+	weight: '100 900',
 });
 
 export const metadata: Metadata = {
-    title: "UniHomes",
-    description: "Real estate platform for UniHomes",
+	title: 'UniHomes',
+	description: 'Real estate platform for UniHomes',
 };
 
 export default function RootLayout({
-    children,
+	children,
 }: Readonly<{
-    children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-    return (
+	return (
         <html lang="en">
-            <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+            <body
+                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+            >
                 <NUIProvider>
-                    <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-                        <TooltipProvider>
-                            <div>{children}</div>
-                           
-                        </TooltipProvider>
+                    <ThemeProvider
+                        attribute="class"
+                        defaultTheme="system"
+                        enableSystem
+                        disableTransitionOnChange
+                    >
+                        <NavBar />
+                        <div>{children}</div>
+                        <Footer />
                     </ThemeProvider>
                 </NUIProvider>
             </body>
