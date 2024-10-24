5f5a7446d808722b98971811764c9bab45996457
rgacorda <rgacorda.the2nd@gmail.com>
2024-10-24 09:10:28 +0800

    feat: restructured layout for admin

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,6 +1,6 @@
 import NavBar from '@/components/navbar/Navbar';
 import Footer from '@/modules/home/components/Footer';
-export default function unauthLayout({
+export default function authLayout({
     children, // will be a page or nested layout
   }: {
     children: React.ReactNode
