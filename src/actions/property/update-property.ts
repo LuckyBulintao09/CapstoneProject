b8260ecd3cad49785b0d16787c56711318213347
pepperonipowered <pepperonipowered@gmail.com>
2024-11-29 06:45:17 +0800

    final addition before checking

--- src/actions/property/update-property.ts
+++ src/actions/property/update-property.ts
@@ -25,3 +25,68 @@ export async function updateProperty(propertyId: string, formValues: CreatePrope
     }
     
 }
+
+export async function updatePropertyTitle(propertyId: string, title: string) {
+    const supabase = createClient();
+    try {
+        const { data, error } = await supabase.from("property").update({ title: title }).eq("id", propertyId);
+        if (error?.code) {
+            throw error
+        }
+        // this is the updated data that is sent to supabase bro
+        return data;
+    } catch (error: any) {
+        throw error
+    }
+    
+}
+
+export async function updatePropertyDescription(propertyId: string, description: string) {
+    const supabase = createClient();
+    try {
+        const { data, error } = await supabase.from("property").update({ description: description }).eq("id", propertyId);
+        if (error?.code) {
+            throw error
+        }
+        // this is the updated data that is sent to supabase bro
+        return data;
+    } catch (error: any) {
+        throw error
+    }
+    
+}
+
+export async function updatePropertyType(propertyId: string, property_type: string) {
+    const supabase = createClient();
+    try {
+        const { data, error } = await supabase.from("property").update({ structure: property_type }).eq("id", propertyId);
+        if (error?.code) {
+            throw error
+        }
+        // this is the updated data that is sent to supabase bro
+        return data;
+    } catch (error: any) {
+        throw error
+    }
+    
+}
+
+export async function updatePropertyLocation(propertyId: string, address:string, location: any) {
+    const supabase = createClient();
+    try {
+        const { data, error } = await supabase.rpc("update_property_address_location", {
+            p_id: propertyId,
+            p_address: address,
+            p_lat: location.lat,            
+            p_lng: location.lng           
+          });
+        if (error?.code) {
+            throw error
+        }
+        // this is the updated data that is sent to supabase bro
+        return data;
+    } catch (error: any) {
+        throw error
+    }
+    
+}
