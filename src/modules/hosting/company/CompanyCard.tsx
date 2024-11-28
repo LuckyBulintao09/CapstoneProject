15df0aac21ebd3ac32605e405f74998b21309163
rgacorda <rgacorda.the2nd@gmail.com>
2024-11-28 15:32:22 +0800

    fix: bugs

--- src/modules/hosting/company/CompanyCard.tsx
+++ src/modules/hosting/company/CompanyCard.tsx
@@ -30,7 +30,7 @@ function CompanyCard() {
         );
     }
 
-    if (true) {
+    if (error || !companies || companies.length === 0) {
         return (
             <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
                 <Card className="col-span-full p-6 text-center bg-gray-50 shadow-md">
