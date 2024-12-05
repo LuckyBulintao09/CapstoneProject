afb191956347f64352720c3c9594ae707b6c0dc4
SheddPugyao <20197474@s.ubaguio.edu>
2024-12-05 10:15:29 +0800

    fix: fixed proprietor padding

--- src/app/(auth)/hosting/properties/[propertyId]/details/@right/description/page.tsx
+++ src/app/(auth)/hosting/properties/[propertyId]/details/@right/description/page.tsx
@@ -16,31 +16,38 @@ async function DescriptionPage({ params }: { params: { propertyId: string } }) {
     const data = await getPropertyDescription(params.propertyId);
 
     return (
-        <section className="flex flex-col h-[calc(100vh-68px)] w-full px-6 airBnbDesktop:overflow-x-hidden airBnbDesktop:overflow-y-auto airBnbTablet:px-10 min-[1128px]:px-20 airBnbBigDesktop:px-0">
-            <header className="airBnbDesktop:mx-auto airBnbDesktop:sticky airBnbDesktop:z-[3] airBnbDesktop:top-0 airBnbDesktop:grow-0 airBnbDesktop:shrink-0 airBnbDesktop:basis-auto airBnbDesktop:pb-5 airBnbDesktop:pt-11 bg-background">
-                <div className="flex items-center justify-between w-full px-10 py-6 airBnbDesktop:gap-6 airBnbDesktop:w-[464px] airBnbDesktop:p-0 airBnbBigDesktop:w-[608px] min-[1128px]:w-[512px]">
-                    <div className="flex justify-center ">
-                        <Link
-                            href={`/hosting/properties`}
-                            className={cn(buttonVariants({ variant: "ghost", size: "icon" }), "rounded-full airBnbDesktop:hidden")}
-                        >
-                            <ArrowLeft className="h-5 w-5" />
-                        </Link>
-                        <h2 className="m-0 p-0 text-[1em]">
-                            <span className="p-1 px-0 text-[2rem] leading-9 tracking-[-0.04rem] font-[500]">Description</span>
-                        </h2>
-                    </div>
-                </div>
-            </header>
+			<section className='flex flex-col h-[calc(100vh-68px)] w-full px-6 airBnbDesktop:overflow-x-hidden airBnbDesktop:overflow-y-auto airBnbTablet:px-10 min-[1128px]:px-20 airBnbBigDesktop:px-0'>
+				<header className='airBnbDesktop:mx-auto airBnbDesktop:sticky airBnbDesktop:z-[3] airBnbDesktop:top-0 airBnbDesktop:grow-0 airBnbDesktop:shrink-0 airBnbDesktop:basis-auto airBnbDesktop:pb-5 airBnbDesktop:pt-11 bg-background'>
+					<div className='mt-3 flex items-center justify-between w-full px-10 py-6 airBnbDesktop:gap-6 airBnbDesktop:w-[464px] airBnbDesktop:p-0 airBnbBigDesktop:w-[800px] min-[1128px]:w-[512px]'>
+						<div className='flex justify-center '>
+							<Link
+								href={`/hosting/properties`}
+								className={cn(
+									buttonVariants({ variant: 'ghost', size: 'icon' }),
+									'rounded-full airBnbDesktop:hidden'
+								)}
+							>
+								<ArrowLeft className='h-5 w-5' />
+							</Link>
+							<h2 className='m-0 p-0 text-[1em]'>
+								<span className='p-1 px-0 text-[1.5rem] font-normal'>
+									Description
+								</span>
+							</h2>
+						</div>
+					</div>
+				</header>
 
-            <div className="flex grow py-6 airBnbDesktop:pt-0 airBnbDesktop:pb-10">
-                <div className="airBnbDesktop:mx-auto airairBnbBigDesktop:w-[608px] min-[1128px]:w-[512px] airBnbDesktop:w-[464px] ">
-                    <PropertyDescriptionForm description={data.description} propertyId={params.propertyId} />
-                </div>
-            </div>
-
-        </section>
-    );
+				<div className='flex grow py-6 airBnbDesktop:pt-0 airBnbDesktop:pb-10'>
+					<div className='airBnbDesktop:mx-auto airBnbBigDesktop:w-[800px] min-[1128px]:w-[512px] airBnbDesktop:w-[464px] '>
+						<PropertyDescriptionForm
+							description={data.description}
+							propertyId={params.propertyId}
+						/>
+					</div>
+				</div>
+			</section>
+		);
 }
 
 export default DescriptionPage;
