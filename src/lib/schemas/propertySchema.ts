380e6c8b4646884e8a53bbfa0f4a148a6459a138
pepperonipowered <pepperonipowered@gmail.com>
2024-12-07 08:12:51 +0800

    removed redundand files and unused files

--- src/lib/schemas/propertySchema.ts
+++ src/lib/schemas/propertySchema.ts
@@ -45,61 +45,49 @@ export const createPropertySchema = z.object({
     
 });
 
-export const createUnitPropertySchema = z.object({
-    property_id: z.string().min(1, { message: "Please select a property." }),
-})
 
-export const createUnitTypeSchema = z.object({
-    unit_structure: z.enum(["apartment", "condominium", "dormitory"], {
-        message: "Please select one of the options.",
-    }),
-    unit_type: z.enum(["room", "shared room", "entire place"], {
-        message: "Please select one of the options.",
+export const propertyTitleSchema = z.object({
+    property_title: z.string().min(1, { message: "Please enter a title." }).max(52, { message: "Title cannot exceed 52 characters." }).regex(/^[a-zA-Z0-9.,'(): -]*$/, {
+        message: "Title can only include letters, numbers, commas, dots, apostrophes, and parentheses.",
     }),
 });
 
-export const createUnitDetailSchema = z.object({
-    unit_occupants: z.number(),
-    unit_bedrooms: z.number(),
-    unit_beds: z.number(),
-});
-
-export const createUnitAmenitySchema = z.object({
-    amenities: z
-        .array(
-            z.object({
-                id: z.number(),
-                amenity_name: z.string(),
-            })
-        )
-        .refine((value) => value.some((item) => item.id && item.amenity_name), {
-            message: "You have to select at least one item.",
-        }),
-    additional_amenities: z
-        .array(
-            z.object({
+export const propertyTypeSchema = z.object({
+    property_type: z.string().min(1, { message: "Please select a property type." }),
+    house_rules: z.array(
+        z.object(
+            {
                 id: z.string(),
-                text: z.string(),
-            })
-        )
-        .optional(),
+                text: z
+                    .string({ message: "Please enter a valid rule." })
+                    .min(1, { message: "House rule name too short. Minimum of 1 character" })
+                    .max(52, { message: "House rule name too long. Max of 52 characters." }),
+            },
+            {
+                message: "House rule object shape is invalid.",
+            }
+        ),
+        {
+            message: "House rule is not an array.", required_error: "Please enter a house rule."
+        }
+    )
 });
 
-export const createUnitTitleSchema = z.object({
-    unit_title: z.string().min(1).max(32),
-    unit_description: z.string().min(1).max(500),
+export const propertyDescriptionSchema = z.object({
+    property_description: z.string().min(1, { message: "Please enter a description." }).max(1000),
 });
 
-// Combine all schemas into a single type
-export const createUnitSchema = z.object({
-    ...createUnitPropertySchema.shape,
-    ...createUnitTypeSchema.shape,
-    ...createUnitDetailSchema.shape,
-    ...createUnitAmenitySchema.shape,
-    ...createUnitTitleSchema.shape,
+export const propertyLocationSchema = z.object({
+    property_address: z.string().min(1, { message: "Please enter an address." }),
+    property_location: z.object({
+        lat: z.number(),
+        lng: z.number(),
+    }).optional(),
 });
-
-// Export the type
-export type CreateUnitType = z.infer<typeof createUnitSchema>;
 
 export type CreatePropertyTypes = z.infer<typeof createPropertySchema>;
+
+export type PropertyTitleData = z.infer<typeof propertyTitleSchema>;
+export type PropertyTypeData = z.infer<typeof propertyTypeSchema>;
+export type PropertyDescriptionData = z.infer<typeof propertyDescriptionSchema>;
+export type PropertyLocationData = z.infer<typeof propertyLocationSchema>;
