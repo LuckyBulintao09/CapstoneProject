8bde5e063d9820e700e3bfb4bb9afe8180992db3
LuckyBulintao09 <luckybbulintao@gmail.com>
2024-12-11 00:14:09 +0800

    company billing unfinished

---
+++ src/modules/admin-dashboard/components/CompanyBillingTab.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { Card, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import React from 'react'
+
+function CompanyBillingTab() {
+  return (
+    <>
+    <Card className='h-full bg-white dark:bg-secondary'>
+    <CardHeader>
+		<CardTitle>Company Billing</CardTitle>
+			<CardDescription>
+                    on-going development -lucky
+		</CardDescription>
+	</CardHeader>
+   </Card>
+    </>
+  )
+}
+
+export default CompanyBillingTab
